76db30a964621b981981bdf6509cc2c6b85a162d
Manuel Lara <manlara@gmail.com>
2018-06-20 16:02:40 -0500

    updated gke version to latest

--- frontend/scripts/build_cluster.sh
+++ frontend/scripts/build_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gcloud  container clusters create $CLUSTERNAME --cluster-version \
- "1.10.4-gke.2" --no-enable-legacy-authorization --image-type "UBUNTU" \
+ "1.10.4-gke.0" --no-enable-legacy-authorization --image-type "UBUNTU" \
    --machine-type "n1-standard-4" \
    --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/pubsub","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
    --network "default" --enable-autoscaling --min-nodes=0 --max-nodes=3 --enable-cloud-logging --enable-cloud-monitoring --subnetwork "default"
